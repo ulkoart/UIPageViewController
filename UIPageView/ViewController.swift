a83b66465730e89dd4010af8031c62f4eb419e57
Artem Ulko <ulkoart@gmail.com>
2020-02-27 14:41:07 +0300

    Initial Commit

---
+++ UIPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by user on 27/02/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
